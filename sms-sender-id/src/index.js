111a261a45abf8d301b33328e61503da429a100e
tawn33y <tawn33y@gmail.com>
2020-09-03 13:47:30 +0300

    Rename client to at

--- sms-sender-id/src/index.js
+++ sms-sender-id/src/index.js
@@ -1,12 +1,12 @@
 import africastalking from 'africastalking';
 import { CONSTANTS } from './constants';
 
-const client = africastalking({
+const at = africastalking({
   username: CONSTANTS.username,
   apiKey: CONSTANTS.apiKey,
 });
 
-client.SMS.send({
+at.SMS.send({
   to: '+254712345678',
   message: 'M2R763TWEF \nConfirmed. You have received KES 50,000.00 from Richie Rich on 14/07/2020 at 09:00AM. New M-MONEY balance is KES 50,000.00',
   from: CONSTANTS.senderId,
